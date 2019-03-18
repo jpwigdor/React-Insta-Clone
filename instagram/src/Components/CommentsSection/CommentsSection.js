78aad5a5c77b3dbe646cbddccbc6b98bb7b60382
James Wigdor <jpwigdor@gmail.com>
2019-03-18 16:05:38 -0400

    Create Add Comment Feature
    its not adding to state during the onSubmit function

--- instagram/src/Components/CommentsSection/CommentsSection.js
+++ instagram/src/Components/CommentsSection/CommentsSection.js
@@ -5,6 +5,26 @@ import Comment from "./Comment";
 import "./CommentsSection.css";
 
 class CommentsSection extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      comments: props.comments,
+      comment: ""
+    };
+  }
+
+  addComment = e => {
+    this.setState({ comment: e.target.value });
+  };
+
+  submitComment = e => {
+    e.preventDefault();
+    const newComment = { text: this.state.comment, username: "JamesWigdor" };
+    const comments = this.state.comments.slice();
+    comments.push(newComment);
+    this.setState({ comments, comment: "" });
+  };
+
   render() {
     return (
       <div className="container-comments-section">
@@ -24,8 +44,13 @@ class CommentsSection extends Component {
           );
         })}
         <div className="time-stamp">{this.props.post.timestamp}</div>
-        <form>
-          <input type="text" placeholder="Add a Comment" />
+        <form className="comments-form" onSubmit={this.submitComment}>
+          <input
+            type="text"
+            placeholder="Add a Comment..."
+            onChange={this.addComment}
+            value={this.comment}
+          />
         </form>
       </div>
     );
