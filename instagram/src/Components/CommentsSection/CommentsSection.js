c03756413625746c1acc0eb623cf2f612a9cffbf
James Wigdor <jpwigdor@gmail.com>
2019-03-18 15:06:09 -0400

    Map out Usernames, Profile Pics, and Likes

--- instagram/src/Components/CommentsSection/CommentsSection.js
+++ instagram/src/Components/CommentsSection/CommentsSection.js
@@ -1,7 +1,18 @@
-import React from "react";
+import React, { Component } from "react";
 
-const CommentsSection = () => {
-  return <div className="container-comments-section" />;
-};
+class CommentsSection extends Component {
+  render() {
+    return (
+      <div className="container-comments-section">
+        <div className="container-comments-icons">
+          {/* Images here */}
+          {/* Images here */}
+          {/* Images here */}
+        </div>
+        <div className="like-count">{this.props.post.likes} Likes</div>
+      </div>
+    );
+  }
+}
 
 export default CommentsSection;
--- instagram/src/Components/PostsContainer/Post.js
+++ instagram/src/Components/PostsContainer/Post.js
@@ -1,13 +1,23 @@
 import React, { Component } from "react";
 import CommentSection from "../CommentsSection/CommentsSection";
 
+import "./Post";
+
 class Post extends Component {
   render() {
     return (
       <div className="conatiner-post">
         <div className="post-header">
           <img src={this.props.post.thumbnailUrl} alt="" />
+          <div className="username">{this.props.post.username}</div>
         </div>
+        <img src={this.props.post.imageUrl} alt="postImg" />
+        <div />
+        <CommentSection
+          comments={this.props.post.comments}
+          post={this.props.post}
+          dummyData={this.props.dummyData}
+        />
       </div>
     );
   }
