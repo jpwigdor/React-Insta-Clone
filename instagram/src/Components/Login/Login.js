b447c3038a2f34da49efafc8820fef4bbb6b0215
James Wigdor <jpwigdor@gmail.com>
2019-03-19 22:42:18 -0400

    Create inputHandler, onChange, value

--- instagram/src/Components/Login/Login.js
+++ instagram/src/Components/Login/Login.js
@@ -11,21 +11,27 @@ class Login extends Component {
     };
   }
 
+  handleInputChange = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
   render() {
     return (
       <div className="login-form">
-        <h2>Instagram</h2>
+        <h2> Instagram </h2>
         <input
           type="text"
           placeholder="Phone number, username, or email"
           name="username"
           value={this.state.username}
+          onChange={this.handleInputChange}
         />
         <input
           type="text"
           placeholder="Password"
           name="password"
           value={this.state.password}
+          onChange={this.handleInputChange}
         />
         <button>Log In</button>
       </div>
