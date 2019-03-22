deb1cf1be13b127f586b3d328f0253311f655028
James Wigdor <jpwigdor@gmail.com>
2019-03-21 22:30:13 -0400

    Completely Style Search Bar with Styled-Components

--- instagram/src/Components/SearchBar/SearchBar.js
+++ instagram/src/Components/SearchBar/SearchBar.js
@@ -1,4 +1,5 @@
 import React from "react";
+import styled from "styled-components";
 
 import logoWord from "../../assets/logo_word.png";
 import camera from "../../assets/logo_camera.jpg";
@@ -11,25 +12,59 @@ import "./SearchBar.css";
 // TODO: Search - SearchBar.js
 //   Set up the search bar will like the comment input and the like button. In your function, filter out any post whose username doesn't match the search term passed in, then update the state with the resulting data.
 
+const StyledSearchBar = styled.div`
+  width: 800px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 10px;
+`;
+
+const StyledSBarLeft = styled.div`
+  justify-content: flex-start;
+`;
+
+const StyledSBarLeftImg = styled.img`
+  height: 40px;
+  padding: 0px 5px;
+`;
+
+const StyledIword = styled(StyledSBarLeftImg)`
+  margin-top: 15px;
+  border-left: 1px solid rgb(119, 119, 119);
+`;
+
+const StyledSBarWrapper = styled.div`
+  border: 1px solid darkgrey;
+  background-color: #f5f8fa;
+`;
+
+const StyledSBarRight = styled.div`
+  display: flex;
+  justify-content: flex-end;
+`;
+
+const StyledSBarRightImg = styled.img`
+  height: 40px;
+`;
+
 const SearchBar = () => {
   return (
-    <div className="container-search-bar">
-      <div className="sbar-left">
-        <div className="logo">
-          <img className="camera" alt="camera" src={camera} />
-          <img className="iword" alt="IGlogo" src={logoWord} />
-        </div>
-      </div>
-      <div className="sbar-wrapper">
+    <StyledSearchBar>
+      <StyledSBarLeft>
+        <StyledSBarLeftImg className="camera" alt="camera" src={camera} />
+        <StyledIword className="iword" alt="IGlogo" src={logoWord} />
+      </StyledSBarLeft>
+      <StyledSBarWrapper>
         <i className="fas fa-search" />
         <input type="text" placeholder="Search" className="sbar" />
-      </div>
-      <div className="sbar-right">
-        <img src={compass} alt="compass" />
-        <img src={like_icon} alt="like icon" />
-        <img src={profile} alt="profile icon" />
-      </div>
-    </div>
+      </StyledSBarWrapper>
+      <StyledSBarRight>
+        <StyledSBarRightImg src={compass} alt="compass" />
+        <StyledSBarRightImg src={like_icon} alt="like icon" />
+        <StyledSBarRightImg src={profile} alt="profile icon" />
+      </StyledSBarRight>
+    </StyledSearchBar>
   );
 };
 
